8dcb9c70b73fa7cb0c89e673477462576fd9bf3a
Darren Liau <liaukaize@gmail.com>
2021-12-21 11:51:06 +0800

    major changes
    removed tkinter
    added global active for logins
    adjusted spellings and declarations
    added better comments
    added more database functions
    added quality of life changes
    refined login function
    added start menu
    added menus for users and admins
    added main function

--- main.py
+++ main.py
@@ -16,10 +16,7 @@
 # Member_4:
 # *********************************************************
 
-#import tkinter for GUI
-from tkinter import *
-#renames "tkinter.messagebox" to tkMessageBox
-import tkinter.messagebox as tkMessageBox
+#_________________________MODULE IMPORTS_____________________________
 #sqlite3 for usage of SQLite tables and database to store data
 import sqlite3
 #hashlib for hashing of passwords
@@ -27,15 +24,31 @@ import hashlib
 #math for mathematical operations
 import math
 
+#_________________________GLOBAL VARIABLES____________________________
+active = ""
+
+#_________________________PASSWORDS____________________________
+# (ADMIN) username:admin password: admin
+# username: 030725101499 password:abc123
+#
+#
+#
+#
+#
+#
+
+#__________________________FUNCTIONS________________________________
+
+#********DATABASE FUNCTIONS********
 #function to create a table named  "user", since data is preloaded, no need to call this function
-def newUserTable():
+def newTableUser():
 
     # connecting to the database
     connection = sqlite3.connect("mysejahtera_0.5.db")
     # cursor
     cursor = connection.cursor()
   
-    # SQL command to create a table in the database
+    # SQL command to create table "user" in the database
     command = """CREATE TABLE user (
     ICnum VARCHAR (13) PRIMARY KEY NOT NULL,
     password VARCHAR (128) NOT NULL,
@@ -44,7 +57,7 @@ def newUserTable():
     phone VARCHAR (12) NOT NULL, 
     address VARCHAR (200) NOT NULL,
     postcode VARCHAR (6) NOT NULL,
-    gender TINYTINT(1),
+    gender TINYINT(1),
     risk TINYINT(2),
     status TINYINT(2),
     userStatus TINYINT(1)
@@ -57,8 +70,57 @@ def newUserTable():
     # close the connection
     connection.close()
 
+#function to create a table named  "ppv", since data is preloaded, no need to call this function
+def newTablePPV():
+    connection = sqlite3.connect("mysejahtera_0.5.db")
+    cursor = connection.cursor()
+  
+    # SQL command to create table "ppv" in the database
+    command = """CREATE TABLE user (
+    ICnum VARCHAR (13) PRIMARY KEY NOT NULL,
+    password VARCHAR (128) NOT NULL,
+    name VARCHAR (100) NOT NULL,
+    age TINYINT(3) NOT NULL,
+    phone VARCHAR (12) NOT NULL, 
+    address VARCHAR (200) NOT NULL,
+    postcode VARCHAR (6) NOT NULL,
+    gender TINYINT(1),
+    risk TINYINT(2),
+    status TINYINT(2),
+    userStatus TINYINT(1)
+    );"""
+
+    cursor.execute(command)
+    connection.commit()
+    connection.close()
+
+#function to export all data in table "user"
+def dataExportUser():
+    connection = sqlite3.connect("mysejahtera_0.5.db")
+    cursor = connection.cursor()
+
+    #Selects everything from table "user"
+    cursor.execute("SELECT * FROM user")
+    output = cursor.fetchall()
+    for i in output:
+       print(i)
+    connection.close()
+
+#function to export all data in table "ppv"
+def dataExportPPV():
+    connection = sqlite3.connect("mysejahtera_0.5.db")
+    cursor = connection.cursor()
+
+    #Selects everything from table "user"
+    cursor.execute("SELECT * FROM ppv")
+    output = cursor.fetchall()
+    for i in output:
+       print(i)
+    connection.close()
+
+#********USER INTERACTION FUNCTIONS********
 #function to input new user's data into table "user"
-def newUser():
+def signup():
     connection = sqlite3.connect("mysejahtera_0.5.db")
     cursor = connection.cursor()
 
@@ -71,7 +133,7 @@ def newUser():
     password = hash_object.hexdigest()
     name = str(input("Full Name: "))
     age = int(input("Age: "))
-    phone = str(input("Phone number: "))
+    phone = str(input("Phone number (without dash): "))
     address = str(input("Address: "))
     postcode = str(input("Postcode: "))
     gender = int(input("Gender (0 for male, 1 for female): "))
@@ -86,33 +148,47 @@ def newUser():
     connection.commit()
     connection.close()
 
-def login():
+    print("Signup Successful")
+    #Redirect to login page
+    login()
 
+#function to login for normal users
+def login():
+    global active
+    connection = sqlite3.connect("mysejahtera_0.5.db")
+    cursor = connection.cursor()
+
+    while True:
+        ICnum = str(input("Enter IC number: "))
+        rawPassword = str(input("Password: "))
+        hash_object = hashlib.sha512(rawPassword.encode())
+        password = hash_object.hexdigest()
+        statement = f"SELECT ICnum from user WHERE ICnum='{ICnum}' AND password = '{password}';"
+        cursor.execute(statement)
+        if not cursor.fetchone():  # An empty result evaluates to False.
+            print("Login failed, please try again.")
+        else:
+            print("Welcome!")
+            active = ICnum
+            connection.close()
+            mainMenu()
+            return active
+
+#function to login for admin
+def loginAdmin():
     ICnum = str(input("Enter IC number: "))
     rawPassword = str(input("Password: "))
     hash_object = hashlib.sha512(rawPassword.encode())
     password = hash_object.hexdigest()
     connection = sqlite3.connect("mysejahtera_0.5.db")
     cursor = connection.cursor()
-    statement = f"SELECT ICnum from user WHERE ICnum='{ICnum}' AND password = '{password}';"
+    statement = f"SELECT ICnum from user WHERE ICnum='{ICnum}' AND password = '{password}' AND userStatus = 1;"
     cursor.execute(statement)
-    if not cursor.fetchone():  # An empty result evaluates to False.
+    if not cursor.fetchone():
         print("Login failed, please try again.")
     else:
         print("Welcome!")
-        mainMenuUser()
-    connection.close()
-
-#function to export all data in table "user"
-def dataExport():
-    connection = sqlite3.connect("mysejahtera_0.5.db")
-    cursor = connection.cursor()
-
-    #Selects everything from table "user"
-    cursor.execute("SELECT * FROM user")
-    output = cursor.fetchall()
-    for i in output:
-       print(i)
+        mainMenuAdmin()
     connection.close()
 
 def risk():
@@ -121,133 +197,59 @@ def risk():
 def status():
     print
 
-def mainMenuUser():
+#********MENUS FUNCTIONS********
+def mainMenu():
+    global active
     connection = sqlite3.connect("mysejahtera_0.5.db")
     cursor = connection.cursor()
-    cursor.execute("SELECT name FROM user WHERE ")
+    cursor.execute(f"SELECT name FROM user WHERE ICnum='{active}'")
     name = cursor.fetchall()
 
-    print("Welcome", name, "please type a number for lorem")
+    print("Welcome", name, "please type a number.")
+    print("Type 1 for vaccination appointments.")
+    print("Type 2 for risk assesment.")
+    print("Type 3 for current COVID-19 updates.")
+    print("Type 4 for updating personal information.")
 
     connection.close()
 
 def mainMenuAdmin():
-    print
+    print("Welcome admin.")
+    print("Type 1 for user management.")
+    print("Type 2 for PPV management.")
+    print("Type 3 for vaccination management.")
+    print("Type 4 for user risk management.")
+    print("Type 5 for data exports.")
 
-'''
-
-#=======================================MAINFRAME=======================================
-root = Tk()
-root.title("Python: Simple Inventory System")
-   
-width = 640
-height = 480
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = (screen_width/2) - (width/2)
-y = (screen_height/2) - (height/2)
-root.geometry("%dx%d+%d+%d" % (width, height, x, y))
-root.resizable(0, 0)
-
-#=======================================LAYOUT=======================================
-
-def LoginForm():
-    global LoginFrame, lbl_result1
-    LoginFrame = Frame(root)
-    LoginFrame.pack(side=TOP, pady=80)
-    lbl_username = Label(LoginFrame, text="Username:", font=('arial', 25), bd=18)
-    lbl_username.grid(row=1)
-    lbl_password = Label(LoginFrame, text="Password:", font=('arial', 25), bd=18)
-    lbl_password.grid(row=2)
-    lbl_result1 = Label(LoginFrame, text="", font=('arial', 18))
-    lbl_result1.grid(row=3, columnspan=2)
-    username = Entry(LoginFrame, font=('arial', 20), textvariable=USERNAME, width=15)
-    username.grid(row=1, column=1)
-    password = Entry(LoginFrame, font=('arial', 20), textvariable=PASSWORD, width=15, show="*")
-    password.grid(row=2, column=1)
-    btn_login = Button(LoginFrame, text="Login", font=('arial', 18), width=35, command=Login)
-    btn_login.grid(row=4, columnspan=2, pady=20)
-    lbl_register = Label(LoginFrame, text="Register", fg="Blue", font=('arial', 12))
-    lbl_register.grid(row=0, sticky=W)
-    lbl_register.bind('<Button-1>', ToggleToRegister)
-
-    def RegisterForm():
-    global RegisterFrame, lbl_result2
-    RegisterFrame = Frame(root)
-    RegisterFrame.pack(side=TOP, pady=40)
-    lbl_username = Label(RegisterFrame, text="Username:", font=('arial', 18), bd=18)
-    lbl_username.grid(row=1)
-    lbl_password = Label(RegisterFrame, text="Password:", font=('arial', 18), bd=18)
-    lbl_password.grid(row=2)
-    lbl_firstname = Label(RegisterFrame, text="Firstname:", font=('arial', 18), bd=18)
-    lbl_firstname.grid(row=3)
-    lbl_lastname = Label(RegisterFrame, text="Lastname:", font=('arial', 18), bd=18)
-    lbl_lastname.grid(row=4)
-    lbl_result2 = Label(RegisterFrame, text="", font=('arial', 18))
-    lbl_result2.grid(row=5, columnspan=2)
-    username = Entry(RegisterFrame, font=('arial', 20), textvariable=USERNAME, width=15)
-    username.grid(row=1, column=1)
-    password = Entry(RegisterFrame, font=('arial', 20), textvariable=PASSWORD, width=15, show="*")
-    password.grid(row=2, column=1)
-    firstname = Entry(RegisterFrame, font=('arial', 20), textvariable=FIRSTNAME, width=15)
-    firstname.grid(row=3, column=1)
-    lastname = Entry(RegisterFrame, font=('arial', 20), textvariable=LASTNAME, width=15)
-    lastname.grid(row=4, column=1)
-    btn_login = Button(RegisterFrame, text="Register", font=('arial', 18), width=35, command=Register)
-    btn_login.grid(row=6, columnspan=2, pady=20)
-    lbl_login = Label(RegisterFrame, text="Login", fg="Blue", font=('arial', 12))
-    lbl_login.grid(row=0, sticky=W)
-    lbl_login.bind('<Button-1>', ToggleToLogin)
-
-#=======================================FUNCTIONS=======================================
-def Exit():
-    result = tkMessageBox.askquestion('System', 'Are you sure you want to exit?', icon="warning")
-    if result == 'yes':
-        root.destroy()
-        exit()
- 
-def ToggleToLogin(event=None):
-    RegisterFrame.destroy()
-    LoginForm()
- 
-def ToggleToRegister(event=None):
-    LoginFrame.destroy()
-    RegisterForm()
- 
-def Register():
-    Database()
-    if USERNAME.get == "" or PASSWORD.get() == "" or FIRSTNAME.get() == "" or LASTNAME.get == "":
-        lbl_result2.config(text="Please complete the required field!", fg="orange")
-    else:
-        cursor.execute("SELECT * FROM `member` WHERE `username` = ?", (USERNAME.get(),))
-        if cursor.fetchone() is not None:
-            lbl_result2.config(text="Username is already taken", fg="red")
+def startMenu():
+    print("-------------------------------------------------------------")
+    print("Welcome to MySejahtera 0.5!")
+    print("Enter 1 for login.")
+    print("Enter 2 for sign up.")
+    print("Enter 3 for admin login.")
+    while True:
+        num = int(input("Enter a number: "))
+        print("-------------------------------------------------------------")
+        if num == 1:
+            login()
+            break
+        elif num == 2:
+            signup()
+            break
+        elif num == 3:
+            loginAdmin()
+            break
         else:
-            cursor.execute("INSERT INTO `member` (username, password, firstname, lastname) VALUES(?, ?, ?, ?)", (str(USERNAME.get()), str(PASSWORD.get()), str(FIRSTNAME.get()), str(LASTNAME.get())))
-            conn.commit()
-            USERNAME.set("")
-            PASSWORD.set("")
-            FIRSTNAME.set("")
-            LASTNAME.set("")
-            lbl_result2.config(text="Successfully Created!", fg="black")
-        cursor.close()
-        conn.close()
-def Login():
-    Database()
-    if USERNAME.get == "" or PASSWORD.get() == "":
-        lbl_result1.config(text="Please complete the required field!", fg="orange")
-    else:
-        cursor.execute("SELECT * FROM `member` WHERE `username` = ? and `password` = ?", (USERNAME.get(), PASSWORD.get()))
-        if cursor.fetchone() is not None:
-            lbl_result1.config(text="You Successfully Login", fg="blue")
-        else:
-            lbl_result1.config(text="Invalid Username or password", fg="red")
-LoginForm()
+            print("invalid")
 
-#========================================MENUBAR WIDGETS==================================
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Exit", command=Exit)
-menubar.add_cascade(label="File", menu=filemenu)
-root.config(menu=menubar)
-'''
+#*****MAIN FUNCTION******
+def main():
+    startMenu()
+    login()
+    log = login()
+    loginUser = login()
+    loginUser = login()
+    if log == True:
+        mainMenu()
+
+main()
